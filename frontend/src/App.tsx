769b98371a00b7b26f7b133582a25070423a9bd6
Dubs2310 <dubs2310@gmail.com>
2024-02-02 19:13:39 -0800

    added antd library

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,19 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  
+
+
   return (
-    <>
-      <div className='w-full h-full bg-gray-300'>
-        <p>THIS IS THE APP PAGE</p>
-      </div>
-      </>
+    // <>
+    //   <div className='w-full h-full bg-gray-300'>
+    //     <p>THIS IS THE APP PAGE</p>
+    //   </div>
+    //   </>
   )
 }
 
