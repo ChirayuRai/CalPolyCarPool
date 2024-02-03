b4443de35ed7f639b5cf917a3104dd7089d06181
ChirayuRai <chirayurai@gmail.com>
2024-02-03 12:41:12 -0800

    trying new structure

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,23 +10,32 @@ import resolvers from './resolvers.js';
 import pool from './dbConn.js';
 
 
-  const app = express();
+const app = express(); 
 
-  const server = new ApolloServer({
-    typeDefs,
-    resolvers
-  })
-    // Note you must call `start()` on the `ApolloServer`
-  // instance before passing the instance to `expressMiddleware`
-  await server.start();
+const httpServer = http.createServer(app);
 
-  // Specify the path where we'd like to mount our server
-  app.use('/graphql', cors(), express.json(), expressMiddleware(server, {
-    context: {
-      db: pool
-    }
-  }));
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+});
+// Ensure we wait for our server to start
+await server.start();
 
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors(),
+  express.json(),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ token: req.headers.token }),
+  }),
+);
 
 
 export default await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
