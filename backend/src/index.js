17df3b2e2dd619139e5e3bfc7f9c172da33378a8
ChirayuRai <chirayurai@gmail.com>
2024-02-03 12:28:38 -0800

    trying minimal index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,39 +10,21 @@ import resolvers from './resolvers.js';
 import pool from './dbConn.js';
 
 
-// npm install @apollo/server express graphql cors
+const main  = async() => {
+  const app = express();
 
-// Required logic for integrating with Express
-const app = express();
-// Our httpServer handles incoming requests to our Express app.
-// Below, we tell Apollo Server to "drain" this httpServer,
-// enabling our servers to shut down gracefully.
-const httpServer = http.createServer(app);
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers
+  })
+    // Note you must call `start()` on the `ApolloServer`
+  // instance before passing the instance to `expressMiddleware`
+  await server.start();
 
-// Same ApolloServer initialization as before, plus the drain plugin
-// for our httpServer.
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
-});
-// Ensure we wait for our server to start
-await server.start();
-
-// Set up our Express middleware to handle CORS, body parsing,
-// and our expressMiddleware function.
-app.use(
-  '/graphql',
-  cors(),
-  // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
-  bodyParser.json({ limit: '50mb' }),
-  // expressMiddleware accepts the same arguments:
-  // an Apollo Server instance and optional configuration options
-  expressMiddleware(server, {
-    context: async ({ req }) => ({ db: pool }),
-  }),
-);
-
-// Modified server startup
-await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
-console.log(`🚀 Server ready at http://localhost:4000/`);
+  // Specify the path where we'd like to mount our server
+  app.use('/graphql', cors(), express.json(), expressMiddleware(server, {
+    context: {
+      db: pool
+    }
+  }));
+}
