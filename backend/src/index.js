47a5b71278acc1901e92a8668421636cbbfdd11c
ChirayuRai <chirayurai@gmail.com>
2024-02-03 12:13:35 -0800

    just running the server

--- backend/src/index.js
+++ backend/src/index.js
@@ -46,4 +46,3 @@ app.use(
 // Modified server startup
 await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
 console.log(`🚀 Server ready at http://localhost:4000/`);
-export default httpServer
