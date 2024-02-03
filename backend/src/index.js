8fd39a0339e6545245e791e0d36d909bafb1d20a
ChirayuRai <chirayurai@gmail.com>
2024-02-03 12:01:00 -0800

    exporting new server

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,21 +1,48 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
+import { expressMiddleware } from '@apollo/server/express4';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
 import typeDefs from './typeDefs.js'
 import resolvers from './resolvers.js';
 import pool from './dbConn.js';
 
+
+// npm install @apollo/server express graphql cors
+
+// Required logic for integrating with Express
+const app = express();
+// Our httpServer handles incoming requests to our Express app.
+// Below, we tell Apollo Server to "drain" this httpServer,
+// enabling our servers to shut down gracefully.
+const httpServer = http.createServer(app);
+
+// Same ApolloServer initialization as before, plus the drain plugin
+// for our httpServer.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
 });
+// Ensure we wait for our server to start
+await server.start();
 
-const vercel_server = await startStandaloneServer(server, {
-  listen: { port: 4000 },
-  context: async ({ req, res }) => ({
-    db: pool,
-  })
-});
+// Set up our Express middleware to handle CORS, body parsing,
+// and our expressMiddleware function.
+app.use(
+  '/',
+  cors(),
+  // 50mb is the limit that `startStandaloneServer` uses, but you may configure this to suit your needs
+  bodyParser.json({ limit: '50mb' }),
+  // expressMiddleware accepts the same arguments:
+  // an Apollo Server instance and optional configuration options
+  expressMiddleware(server, {
+    context: async ({ req }) => ({ db: pool }),
+  }),
+);
 
-console.log(`🚀  Server ready at: ${vercel_server.url}`);
-export default ApolloServer
-
+// Modified server startup
+await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+console.log(`🚀 Server ready at http://localhost:4000/`);
+export default httpServer
