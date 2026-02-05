e9a487873f9002639e277417be26faf3059e611f
Abdul Kalam Syed <abdulksyed10@gmail.com>
2026-02-04 22:40:06 -0800

    updating registration

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -69,6 +69,7 @@ const App: React.FC = () => {
 
   return (
     <div className="App">
+      {/* <Registration/> */}
       {/* <Flex gap="middle" wrap="wrap"> */}
         <Layout style={layoutStyle}>
           <AppHeader />
